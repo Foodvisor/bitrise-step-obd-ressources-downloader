7140906483827d4e3482a8eb0e8ad5c961b5e08f
Louise Foodvisor <louise@foodvisor.io>
2023-07-19 10:19:34 +0200

    Add version and env input to script

--- step.sh
+++ step.sh
@@ -2,4 +2,4 @@
 set -ex
 git clone git@github.com:Foodvisor/obd-json-downloader.git
 xcodebuild -project obd-json-downloader/obd-json-downloader.xcodeproj -configuration Release
-obd-json-downloader/build/Release/obd-json-downloader -mail "${mail}" -password "${password}"
+obd-json-downloader/build/Release/obd-json-downloader -mail "${mail}" -password "${password}" -env "${env}" -version "${version}"
